753472df701c09115c6c581b4fb041ed28b35929
Lichanyeok <cksgur956@naver.com>
2024-03-17 19:02:35 +0900

    first_commit

---
+++ src/main/java/lichanyeok/jumper/JumperApplication.java
@@ -0,0 +1,13 @@
+package lichanyeok.jumper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JumperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lichanyeok/jumper/JumperApplicationTests.java
@@ -0,0 +1,13 @@
+package lichanyeok.jumper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
